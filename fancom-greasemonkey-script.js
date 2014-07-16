cdc279ddeab348e4748b459eb0f06adec9d4bf5b
Michal Budzynski <michal@virtualdesign.pl>
2014-07-16 22:29:09 +0200

    [FANCOM] - remove gm script

--- fancom-greasemonkey-script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-setInterval(function(){
-  var inputValue = document.getElementById('pull_request_title').value;
-  inputValue = inputValue.replace(/:(.*?):/gi, '');
-}, 200);
-
-alert('test');
